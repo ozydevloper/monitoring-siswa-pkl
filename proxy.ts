d78a66aede80afbbd88168c5df5c5f7c12aef108
ozydevloper <ozydeveloper@gmail.com>
2026-03-06 09:45:10 +0700

    benerin lagi 1

--- proxy.ts
+++ proxy.ts
@@ -2,48 +2,49 @@ import { NextAuthRequest } from "next-auth";
 import { auth } from "./auth";
 import { NextResponse } from "next/server";
 
-export default async function proxy(request: NextAuthRequest) {
-  const session = await auth();
-
+export default auth(async (request: NextAuthRequest) => {
   if (request.nextUrl.pathname.startsWith("/api/query")) {
     const signature = await request.headers.get("nothing-to-see");
     if (!signature) return NextResponse.json({ data: "Request ga lengkap" });
     NextResponse.next();
   }
 
-  if (request.nextUrl.pathname === "/" && !session) {
+  if (request.nextUrl.pathname === "/" && !request.auth) {
     return NextResponse.redirect(new URL("/login/siswa", request.url));
   }
 
-  if (request.nextUrl.pathname === "/" && session?.user?.role === "GURU") {
+  if (request.nextUrl.pathname === "/" && request.auth?.user?.role === "GURU") {
     return NextResponse.redirect(new URL("/monitoring", request.url));
   }
 
-  if (request.nextUrl.pathname.startsWith("/logout") && !session) {
+  if (request.nextUrl.pathname.startsWith("/logout") && !request.auth) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  if (request.nextUrl.pathname === "/" && session?.user?.role === "SISWA") {
+  if (
+    request.nextUrl.pathname === "/" &&
+    request.auth?.user?.role === "SISWA"
+  ) {
     return NextResponse.redirect(new URL("/siswa", request.url));
   }
 
-  if (request.nextUrl.pathname.startsWith("/login") && session) {
+  if (request.nextUrl.pathname.startsWith("/login") && request.auth) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
   if (
     request.nextUrl.pathname.startsWith("/siswa") &&
-    session?.user?.role === "GURU"
+    request.auth?.user?.role === "GURU"
   ) {
     return NextResponse.redirect(new URL("/monitoring", request.url));
   }
   if (
     request.nextUrl.pathname.startsWith("/monitoring") &&
-    session?.user?.role === "SISWA"
+    request.auth?.user?.role === "SISWA"
   ) {
     return NextResponse.redirect(new URL("/siswa", request.url));
   }
-  if (request.nextUrl.pathname.startsWith("/auth/error") && session) {
+  if (request.nextUrl.pathname.startsWith("/auth/error") && request.auth) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-}
+});
